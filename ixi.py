98294f13a2715e9a776dce8abf890247fbd3c4a2
Adam Selker <adamselker@gmail.com>
2022-11-09 20:43:40 -0800

    Intermission: colors!

--- ixi.py
+++ ixi.py
@@ -1,4 +1,5 @@
 import numpy as np
+from colorama import Fore, Back, Style
 
 import cxc
 
@@ -10,7 +11,7 @@ import cxc
 # By definition, it's always 1's turn next.
 
 
-def pretty_print(ixi_, whose_turn="x"):
+def pretty_print(ixi_, whose_turn="x", prev_move=None):
     if whose_turn == "x":
         marks = {0: " ", 1: "x", -1: "o", 2: "?"}
     elif whose_turn == "o":
@@ -27,10 +28,17 @@ def pretty_print(ixi_, whose_turn="x"):
         for col in [3, 7]:
             grid[i].insert(col, vline)
 
+    # Color the most recent move
+    if prev_move is not None:
+        row = prev_move[0] * 3 + prev_move[2]
+        col = prev_move[1] * 4 + prev_move[3]
+        grid[row].insert(col, Fore.RED)
+        grid[row].insert(col + 2, Fore.WHITE)
+
     for i in [3, 7]:
         grid.insert(i, hline * 3 + cross + hline * 3 + cross + hline * 3)
 
-    return "\n".join(("".join(row) for row in grid))
+    return Fore.WHITE + "\n".join(("".join(row) for row in grid))
 
 
 def victory_state(ixi_):
