44bd78adaf849c592efc10a54cfb17e94d599e3d
Adam Selker <adam.selker@joby.aero>
2022-11-17 16:41:55 -0800

    blerp

--- nn_common.py
+++ nn_common.py
@@ -1,9 +1,11 @@
 from tensorflow import keras
 
+l1 = 1e-5
+l2 = 1e-4
 
 def make_model():
     return keras.models.Sequential(
-        [keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.L1L2(), input_shape=(81,))]
-        + [keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.L1L2()) for _ in range(7)]
+        [keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.L1L2(l1=l1, l2=l2), input_shape=(81,))]
+        + [keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.L1L2(l1=l1, l2=l2)) for _ in range(7)]
         + [keras.layers.Dense(1, activation="tanh")]
     )
--- tournament.py
+++ tournament.py
@@ -2,6 +2,7 @@ import sys
 import pickle
 import numpy as np
 import itertools
+from pathlib import Path
 
 from state import State
 import ixi
@@ -12,7 +13,7 @@ from mcts import MctsBot, SimpleNnBot
 Round-robin tournament.
 """
 
-NUM_GAMES_PER_MATCHUP = 256
+NUM_GAMES_PER_MATCHUP = 4
 NUM_PREFILLED_EACH = 18
 SOMETIMES_UNEQUAL = False
 
@@ -40,6 +41,8 @@ def main():
 
     bots = [MctsBot(30), MctsBot(35)]
     # bots = [MctsBot(50), SimpleNnBot("model.h5")]
+    # bots = [SimpleNnBot("model.h5"), SimpleNnBot("model.h5")]
+    # bots[1].name += "_the_other"
 
     # Make sure names are unique
     assert len([bot.name for bot in bots]) == len({bot.name for bot in bots})
@@ -47,58 +50,59 @@ def main():
     # All pairs, in alphabetical order
     matchups = list(itertools.combinations(bots, 2))
 
-    # Play the round robin
-    histories = []
-    for bot1, bot2 in matchups:
+    for epoch in itertools.count():
+        # Play the round robin
+        histories = []
+        for bot1, bot2 in matchups:
+            for game_index in range(NUM_GAMES_PER_MATCHUP):
+                if game_index % 2:
+                    even_bot = bot1
+                    odd_bot = bot2
+                else:
+                    even_bot = bot2
+                    odd_bot = bot1
 
-        for game_index in range(NUM_GAMES_PER_MATCHUP):
-            if game_index % 2:
-                even_bot = bot1
-                odd_bot = bot2
+                state_ = generate_partially_full_state()
+                history = ((even_bot.name, odd_bot.name), [state_.copy()])
+                histories.append(history)
+
+                for turn_index in itertools.count():
+                    state_.move((odd_bot if turn_index % 2 else even_bot).get_move(state_))
+                    history[1].append(state_.copy())
+                    victory_state = state_.victory_state()
+                    if victory_state:
+                        break
+
+        filename = Path(sys.argv[1]) / (str(epoch) + ".pkl")
+        with open(filename, "wb") as f:
+            # pickle.dump([h[1] for h in histories], f)
+            pickle.dump(histories, f)
+
+        # Calculate and display tournament results.
+        # There's gotta be a better way to do this.  A dict isn't quite the right data structure.  Or maybe tournament_results should map from every pairing including duplicates.
+        tournament_results = {(bot1.name, bot2.name): [0, 0, 0] for bot1, bot2 in matchups}
+        for history in histories:
+            even_name, odd_name = history[0]
+            if (even_name, odd_name) in tournament_results:
+                relevant_results = tournament_results[(even_name, odd_name)]
+                swap = False
             else:
-                even_bot = bot2
-                odd_bot = bot1
+                relevant_results = tournament_results[(odd_name, even_name)]
+                swap = True
 
-            state_ = generate_partially_full_state()
-            history = ((even_bot.name, odd_bot.name), [state_.copy()])
-            histories.append(history)
-
-            for turn_index in itertools.count():
-                state_.move((odd_bot if turn_index % 2 else even_bot).get_move(state_))
-                history[1].append(state_.copy())
-                victory_state = state_.victory_state()
-                if victory_state:
-                    break
-
-    with open(sys.argv[1], "wb") as f:
-        # pickle.dump([h[1] for h in histories], f)
-        pickle.dump(histories, f)
-
-    # Calculate and display tournament results.
-    # There's gotta be a better way to do this.  A dict isn't quite the right data structure.  Or maybe tournament_results should map from every pairing including duplicates.
-    tournament_results = {(bot1.name, bot2.name): [0, 0, 0] for bot1, bot2 in matchups}
-    for history in histories:
-        even_name, odd_name = history[0]
-        if (even_name, odd_name) in tournament_results:
-            relevant_results = tournament_results[(even_name, odd_name)]
-            swap = False
-        else:
-            relevant_results = tournament_results[(odd_name, even_name)]
-            swap = True
-
-        if history[1][-1].victory_state() == 2:
-            relevant_results[2] += 1
-        elif len(history) % 2:
-            if swap:
-                relevant_results[0] += 1
+            if history[1][-1].victory_state() == 2:
+                relevant_results[2] += 1
+            elif len(history) % 2:
+                if swap:
+                    relevant_results[0] += 1
+                else:
+                    relevant_results[1] += 1
             else:
-                relevant_results[1] += 1
-        else:
-            if swap:
-                relevant_results[1] += 1
-            else:
-                relevant_results[0] += 1
-    print(tournament_results)
+                if swap:
+                    relevant_results[1] += 1
+                else:
+                    relevant_results[0] += 1
+        print(tournament_results)
 
 
 if __name__ == "__main__":
